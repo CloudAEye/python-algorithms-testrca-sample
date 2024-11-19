fc936856707d8a43df27af62ffd3e7de007dab7b
deepakmeenax <deepak@cloudaeye.com>
2024-11-19 15:03:56 +0530

    change4s to retirger tests

--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -16,7 +16,7 @@ Find an element in a sorted array (in ascending order).
 
 def binary_search(array, query):
     """
-    Worst-case Complexity: O(log(n))
+    Worst-case Complexity : O(log(n))
 
     reference: https://en.wikipedia.org/wiki/Binary_search_algorithm
     """
@@ -38,7 +38,7 @@ def binary_search(array, query):
 #In this below function we are passing array, it's first index , last index and value to be searched
 def binary_search_recur(array, low, high, val):
     """
-    Worst-case Complexity: O(log(n))
+    Worst-case Complexity : O(log(n))
 
     reference: https://en.wikipedia.org/wiki/Binary_search_algorithm
     """
